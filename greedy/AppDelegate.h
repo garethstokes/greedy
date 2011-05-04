6718ede30505fa759aa4f58fc9630111974495c0
richard_owen <richard_owen@digitalfivesydney.com>
2011-05-04 19:05:22 +1000

    Brand New Project

---
+++ greedy/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  greedy
+//
+//  Created by Richard Owen on 4/05/11.
+//  Copyright Digital Five 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ greedy/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  greedy
+//
+//  Created by Richard Owen on 4/05/11.
+//  Copyright Digital Five 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
